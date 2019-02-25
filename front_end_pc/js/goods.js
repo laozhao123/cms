414c0b613c2920f8f0591a499929907755a3a9a1
zhao <aa@qq.com>
2019-02-25 17:24:04 +0800

    商品首页

--- front_end_pc/js/goods.js
+++ front_end_pc/js/goods.js
@@ -14,13 +14,27 @@ var vm = new Vue({
 		//获取推荐商品
         get_recommend_goods: function () {
            //发送请求
-                })
+            axios.get('http://127.0.0.1:8000/goods/top/')
+               .then(response=>{
+                this.recommend_goods=response.data['is_red'];
+
+
+           }).catch(error=>{
+                console.log(error.response);
+           })
         },
 		//获取分类商品
         get_category_goods: function () {
-           //发送请求
-                })
-        },
+            //发送请求
+            axios.get('http://127.0.0.1:8000/goods/cate/')
+               .then(response=>{
+                this.categories=response.data;
+
+
+           }).catch(error=>{
+                console.log(error.response);
+           })
+        }
     },
 
     filters: {
