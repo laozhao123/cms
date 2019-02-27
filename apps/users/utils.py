f2f06f543aa7cbcaf6adc8c43c56d317310c7034
zhao <aa@qq.com>
2019-02-27 18:41:53 +0800

    用户登录(utils)

---
+++ apps/users/utils.py
@@ -0,0 +1,31 @@
+#默认只返回 token
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+
+from users.models import User
+
+
+def jwt_response_payload_handler(token, user=None, request=None):
+
+    return {
+        'token': token,
+        'username':user.username,
+        'id':user.id
+    }
+
+
+class UsernameMobileAuthBackend(ModelBackend):
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        """
+        判断用户名(手机号)或者密码是否正确，返回对应的用户对象。
+        """
+        query_set=User.objects.filter(Q(username=username) | Q(mobile=username))
+
+        try:
+            if query_set.exists():
+                user=query_set.get() # 取出唯一的一条数据（取不到或者有多条数据都会出错）
+                if user.check_password(password):  # 进入一步判断密码是否正确
+                    return user
+        except:
+            return None
